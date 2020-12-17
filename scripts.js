78871b98d112b46122afc5c93a6e40f7adfb8085
Blair Brown <blair.j.brown@gmail.com>
2020-12-17 17:34:19 -0500

    finished xmas tree exercise

--- scripts.js
+++ scripts.js
@@ -1,25 +1,34 @@
-{
-  const colors = ["silver", "red", "blue"];
+const colors = ["silver", "red", "blue"];
+let christmasTree = document.querySelector('.christmas-tree');
 
-  function addPresent() {
-    // Create DOM element and set it to a variable
+function addPresent() {
+  // Create DOM element and set it to a variable
+  // let christmasTree = document.querySelector('.christmas-tree');
+  let newPresent = document.createElement('div');
 
-    // Add class "present"
+  // Add class "present"
+  newPresent.classList.add("present");
+  // Add a class for color,
+  // randomly adding either class
+  // "silver", "red", or "blue".
+  // Use the colors array.
 
-    // Add a class for color,
-    // randomly adding either class
-    // "silver", "red", or "blue".
-    // Use the colors array.
+  let randomNum = Math.floor(Math.random() * 3);
+  newPresent.classList.add(colors[randomNum]);
+  // Add event listener so when
+  // the DOM element is clicked,
+  // it is removed from the document
 
-    // Add event listener so when
-    // the DOM element is clicked,
-    // it is removed from the document
+  newPresent.addEventListener("click", function() {
+    newPresent.remove();
+  })
 
-    // Append your DOM element to the ".presents" div
-
-  }
-
-  // Add event listener so when .christmas-tree
-  // is clicked, addPresent() is called
-
+  // Append your DOM element to the ".presents" div
+  let presents = document.querySelector(".presents");
+  presents.appendChild(newPresent);
 }
+
+// Add event listener so when .christmas-tree
+// is clicked, addPresent() is called
+
+document.querySelector(".christmas-tree").addEventListener("click", addPresent);
